f2ee0096213bd4e4468e63eca04eca58017c162b
marcos ferreira <marcos.ferreira@koltin.mx>
2025-10-15 04:28:05 -0300

    Fix horizon scheduler quick deploy integration (#30)
    * Fix laravel horizon, scheduler and quick deploy integration
    
    We were matching on 200 but it is 201
    
    * Use bats tmp dir instead of using a `mktemp`
    
    Which was emitting ugly warnings on mac

--- entrypoint.sh
+++ entrypoint.sh
@@ -964,7 +964,7 @@ if [[ $INPUT_HORIZON_ENABLED == 'true' ]]; then
     echo ""
   fi
 
-  if [[ $HTTP_STATUS -eq 200 ]]; then
+  if [[ $HTTP_STATUS -eq 200 || $HTTP_STATUS -eq 201 ]]; then
     echo "Laravel Horizon integration enabled successfully"
   else
     echo "Failed to enable Laravel Horizon integration. HTTP status code: $HTTP_STATUS"
@@ -1002,7 +1002,7 @@ if [[ $INPUT_SCHEDULER_ENABLED == 'true' ]]; then
     echo ""
   fi
 
-  if [[ $HTTP_STATUS -eq 200 ]]; then
+  if [[ $HTTP_STATUS -eq 200 || $HTTP_STATUS -eq 201 ]]; then
     echo "Laravel Scheduler integration enabled successfully"
   else
     echo "Failed to enable Laravel Scheduler integration. HTTP status code: $HTTP_STATUS"
@@ -1040,7 +1040,7 @@ if [[ $INPUT_QUICK_DEPLOY_ENABLED == 'true' ]]; then
     echo ""
   fi
 
-  if [[ $HTTP_STATUS -eq 200 ]]; then
+  if [[ $HTTP_STATUS -eq 200 || $HTTP_STATUS -eq 201 ]]; then
     echo "Enable quick deployment successfully"
   else
     echo "Failed to enable quick deployment. HTTP status code: $HTTP_STATUS"
